1ce9959bd8e7adebc716de29c07655ff13301f31
IvanFrezzaJr <ivanfrezza@gmail.com>
2024-11-10 22:26:09 +0000

    feat: add observer

--- ProjectManagementSystem/Program.cs
+++ ProjectManagementSystem/Program.cs
@@ -1,2 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+public class Event
+{
+    public Role Role { get; set; }
+    public string Action { get; set; }
+    public string Message { get; set; }
+    public DateTime CreatedAt { get; } = DateTime.Now;
+
+    public Event(Role role, string action, string message)
+    {
+        Role = role;
+        Action = action;
+        Message = message;
+    }
+}
+
+public interface ISubscriber
+{
+    void Update(Event @event);
+}
+
+public interface IPublisher
+{
+    void AddSubscriber(ISubscriber subscriber);
+    void NotifyObservers(Event @event);
+}
+
+
+public abstract class Subscriber : ISubscriber
+{
+    public abstract void Update(Event @event);
+}
+
+
+
+public abstract class Publisher : IPublisher
+{
+    private List<ISubscriber> _subscribers = new List<ISubscriber>();
+    
+    public void AddSubscriber(ISubscriber subscriber)
+    {
+        
+        if (!this._subscribers.Contains(subscriber)) {
+            this._subscribers.Add(subscriber);
+        }
+    }
+
+    public void NotifyObservers(Event @event) {
+        foreach (var subscriber in this._subscribers)
+        {
+            subscriber.Update(@event);
+        }
+    }
+
+}
+
+
+public class Role : Publisher
+{
+    public string Name { get; set; }
+
+    public Role(string name)
+    {
+        Name = name;
+    }
+}
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+       
+    }
+}
