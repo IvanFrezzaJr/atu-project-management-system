e8a3ff361159609e48c8d2c001bc7b54a21db075
IvanFrezzaJr <ivanfrezza@gmail.com>
2024-11-10 22:26:55 +0000

    feat: add Admin and Student classes

--- ProjectManagementSystem/Program.cs
+++ ProjectManagementSystem/Program.cs
@@ -67,10 +67,57 @@ public class Role : Publisher
 }
 
 
+public class Student : Role
+{
+    public Student(string name) : base(name) { }
+
+    public void Submit(string message)
+    {
+        System.Console.WriteLine($"{Name} has submiting: {message}");
+
+        Event @event = new Event(this, "Print", message);
+        this.NotifyObservers(@event);
+    }
+}
+
+public class Admin : Subscriber
+{
+    private string _role = string.Empty;
+    private List<Event> _logs = new List<Event>();
+
+    public Admin(string Admin)
+    {
+        this._role = Admin;
+    }
+
+    public override void Update(Event @event)
+    {
+       this._logs.Add(@event);
+    }
+
+    public void PrintLogs()
+    {
+        foreach (var log in this._logs)
+        {
+            System.Console.WriteLine($"[{log.CreatedAt}] - {log.Role.Name}.{log.Action}: {log.Message}");
+        }
+    }
+}
+
+
 internal class Program
 {
     static void Main(string[] args)
     {
-       
+        Student ivan = new Student("Ivan");
+        Student bruna = new Student("Bruna");
+        Admin admin = new Admin("Super admin");
+        ivan.AddSubscriber(admin);
+        bruna.AddSubscriber(admin);
+
+        ivan.Submit("Hello");
+        bruna.Submit("World");
+
+        admin.PrintLogs();
     }
 }
