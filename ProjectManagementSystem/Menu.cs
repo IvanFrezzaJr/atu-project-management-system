4a922c8bfe6df8ffbce32c90bf86fabe5beb84de
IvanFrezzaJr <ivanfrezza@gmail.com>
2024-11-12 22:10:38 +0000

    feat: add basic menu structure

---
+++ ProjectManagementSystem/Menu.cs
@@ -0,0 +1,139 @@
+﻿namespace ProjectManagementSystem
+{
+
+
+    interface IMenuItem
+    {
+        abstract void Execute();
+    }
+
+    interface IMenu
+    {
+        void Show();
+        void AddItem(IMenuItem item);
+        void AddSubMenu(Menu submenu);
+    }
+
+
+    public abstract class MenuItem : IMenuItem
+    {
+        string _name = string.Empty;
+
+        public MenuItem(string name)
+        {
+            this._name = name;
+        }
+
+        public abstract void Execute();
+    }
+
+
+
+    public class Option1MenuItem : MenuItem
+    {
+        public Option1MenuItem(string name) : base(name) { }
+
+        public override void Execute()
+        {
+            System.Console.WriteLine("Option1MenuItem");
+        }
+    }
+
+    public class Option2MenuItem : MenuItem
+    {
+        public Option2MenuItem(string name) : base(name) { }
+
+        public override void Execute()
+        {
+            System.Console.WriteLine("Option2MenuItem");
+        }
+    }
+
+
+    public class Option3MenuItem : MenuItem
+    {
+        public Option3MenuItem(string name) : base(name) { }
+
+        public override void Execute()
+        {
+            System.Console.WriteLine("Option3MenuItem");
+        }
+    }
+
+
+    class Menu : IMenu
+    {
+        string _title = string.Empty;
+        List<IMenuItem> _items = new List<IMenuItem>();
+        List<IMenu> _submenus = new List<IMenu>();
+        List<object> _current = new List<object>();
+        Menu? parent = null;
+
+        public Menu(string title)
+        {
+            this._title = title;
+        }
+
+        public void AddItem(IMenuItem item)
+        {
+            this._items.Add(item);
+            this.UpdateCurrent();
+
+        }
+
+        public void AddSubMenu(Menu submenu)
+        {
+            submenu.parent = this;
+            this._submenus.Add(submenu);
+            this.UpdateCurrent();
+        }
+
+        public void DisplayOptions()
+        {
+            for (int i = 0; i < this._current.Count; i++)
+            {
+                System.Console.WriteLine($"{this._current[i]}");
+            }
+        }
+
+        public bool HandleChoice(int option)
+        {
+            if (option == 0)
+            {
+                return false;
+            } else
+            {
+                Console.WriteLine($"{option}");
+                return true;
+            }
+        }
+
+        public void Show()
+        {
+            while (true)
+            {
+                try
+                {
+                    int option = int.Parse(Console.ReadLine());  // Lê a entrada e converte para int
+                    Console.WriteLine($"Input: {option}");
+                    bool result = this.HandleChoice(option);
+                    if (!result) {
+                        break;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input! Please enter an integer.");
+                }
+            }
+          
+        }
+
+        private void UpdateCurrent()
+        {
+            this._current.AddRange(this._items);
+            this._current.AddRange(this._submenus);
+        }
+
+    }
+}
--- ProjectManagementSystem/Program.cs
+++ ProjectManagementSystem/Program.cs
@@ -11,20 +11,43 @@ internal class Program
     /// <param name="args">Command line arguments (not used in this program).</param>
     static void Main(string[] args)
     {
-        // Create instances of students and an admin
-        Student ivan = new Student("Ivan");
-        Student bruna = new Student("Bruna");
-        Admin admin = new Admin("Super admin");
+        //// Create instances of students and an admin
+        //Student ivan = new Student("Ivan");
+        //Student bruna = new Student("Bruna");
+        //Admin admin = new Admin("Super admin");
 
-        // Subscribe the admin to both students
-        ivan.AddSubscriber(admin);
-        bruna.AddSubscriber(admin);
+        //// Subscribe the admin to both students
+        //ivan.AddSubscriber(admin);
+        //bruna.AddSubscriber(admin);
 
-        // Students submit events
-        ivan.Submit("Hello");
-        bruna.Submit("World");
+        //// Students submit events
+        //ivan.Submit("Hello");
+        //bruna.Submit("World");
 
-        // Admin prints the logs of the events it received
-        admin.PrintLogs();
+        //// Admin prints the logs of the events it received
+        //admin.PrintLogs();
+
+        MenuItem option1 = new Option1MenuItem("Option 1");
+        MenuItem option2 = new Option2MenuItem("Option 2");
+        MenuItem option3 = new Option3MenuItem("Option 3");
+
+        //Criando menus e submenus
+        Menu mainMenu = new Menu("Main menu");
+        mainMenu.AddItem(option1);
+        mainMenu.AddItem(option2);
+
+        // Submenu de exemplo
+        Menu submenu = new Menu("Submenu example");
+        submenu.AddItem(option1);
+        submenu.AddItem(option2);
+
+        // Adiciona o submenu ao menu principal
+        mainMenu.AddSubMenu(submenu);
+        mainMenu.AddItem(option3);
+
+        // Exibindo o menu principal
+        mainMenu.Show();
+
+
     }
 }
