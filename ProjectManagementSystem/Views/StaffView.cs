8bdaa9a8113eef73060b54fd0208ad4d521dd956
IvanFrezzaJr <ivanfrezza@gmail.com>
2024-12-14 22:48:10 +0000

    clean: remove unused method

--- ProjectManagementSystem/Views/StaffView.cs
+++ ProjectManagementSystem/Views/StaffView.cs
@@ -7,28 +7,10 @@ namespace ProjectManagementSystem.Views
     // Class responsible for handling user input/output
     public class StaffView : BaseView
     {
-        public void ShowSubmissionsResult(List<dynamic> submissions)
-        {
-            foreach (var submission in submissions)
-            {
-                Console.WriteLine($"Student: {submission.StudentName}");
-                Console.WriteLine($"Assessment: {submission.AssessmentDescription}");
-                Console.WriteLine($"Score: {submission.ScoreStatus} / {submission.MaxScore}");
-                Console.WriteLine(new string('-', 30)); // Separator line
-            }
-        }
-
         public void DisplayClassroomInfo(Classroom classroom)
         {
             Console.WriteLine($"ClassName: {classroom.Name}");
         }
-
-
-
-
-
-
-        
 
     }
 }
